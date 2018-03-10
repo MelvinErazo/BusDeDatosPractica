75952308617abf6ab9dfc51caa2b4e1622fa89a9
MelvinErazo <melvin.erazo@hotmail.com>
2018-03-10 10:49:37 -0600

    primer codigo arduino

---
+++ Arduino_Codigo/Arduino_Codigo.ino
@@ -0,0 +1,52 @@
+#define BuscarLed 1
+#define BuscarNumero 2
+
+
+int Led = 0;
+int Dato = 0;
+int EstadoBusqueda = 0;
+
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+  Serial.println("Hola soy un Arduino");
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+ if(Serial.available()) {
+  char Letra = Serial.read();
+if(Letra == "L"){
+  EstadoBusqueda = BuscarLed;
+  }
+  else if (Letra == "D") {
+    EstadoBusqueda = BuscarNumero;
+  }
+ else if (Letra == ".") {
+  analogWrite(Led, Dato);
+  Serial.print("El les es: ");
+  Serial.print(Led);
+  Serial.print("dato : ");
+  Serial.println(Dato);
+  Led = 0;
+  Dato = 0;
+ }
+ else if (Letra >= '0' && Letra <= '9') {
+ int NumeroActual = int (Letra - '0');
+ if (EstadoBusqueda == BuscarLed) {
+  Led = Led * 10 + NumeroActual;
+ }
+ else if (EstadoBusqueda == BuscarNumero) {
+  Dato = Dato * 10 + NumeroActual;
+ }
+
+ }
+
+else {
+    Led = 0;
+  Dato = 0;
+}
+}
+ }
+
